dca46ddb8d2b3c73db14e066322e4d56a8027f10
kazk <kazk.dev@gmail.com>
2021-06-18 14:16:28 -0700

    Log when ready

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,6 +1,7 @@
 import { Client } from "discord.js";
 
 export const makeOnReady = (bot: Client) => async () => {
+  console.log("ready!");
   await bot.user?.setPresence({
     status: "online",
   });
