038edf38f18191ba068d760d37cdae9b71740b39
kazk <kazk.dev@gmail.com>
2021-06-18 13:40:47 -0700

    Move Jest config into package.json

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  testEnvironment: "node",
-  transform: {
-    "^.+\\.ts$": "ts-jest",
-  },
-  moduleFileExtensions: ["ts", "js"],
-  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.[jt]s$",
-};
