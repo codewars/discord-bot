b5766b81e0487e0dda79a125d64c4d33325de0b5
kazk <kazk.dev@gmail.com>
2021-06-23 14:14:29 -0700

    Clean up command arguments extraction

--- src/events/message/commands/types.ts
+++ src/events/message/commands/types.ts
@@ -1,9 +1,46 @@
-import { Message } from "discord.js";
+import { Message, Client, TextChannel } from "discord.js";
+import { z } from "zod";
 // reexport for convenience
 export { Message };
 
-// Argument validation should happen in each command with common helper functions.
-// On error, commands can post a help message that's deleted after some time.
+const wordSchema = () =>
+  z.object({
+    type: z.literal("word"),
+    word: z.string(),
+  });
+
+const codeBlockSchema = () =>
+  z.object({
+    type: z.literal("code"),
+    info: z.string(),
+    code: z.string(),
+  });
+
+const userTagSchema = () =>
+  z.object({
+    type: z.literal("user"),
+    id: z.string(),
+  });
+
+const roleTagSchema = () =>
+  z.object({
+    type: z.literal("role"),
+    id: z.string(),
+  });
+
+const channelTagSchema = () =>
+  z.object({
+    type: z.literal("channel"),
+    id: z.string(),
+  });
+
+export type Word = z.infer<ReturnType<typeof wordSchema>>;
+export type CodeBlock = z.infer<ReturnType<typeof codeBlockSchema>>;
+export type UserTag = z.infer<ReturnType<typeof userTagSchema>>;
+export type RoleTag = z.infer<ReturnType<typeof roleTagSchema>>;
+export type ChannelTag = z.infer<ReturnType<typeof channelTagSchema>>;
+export type CommandArg = Word | CodeBlock | UserTag | RoleTag | ChannelTag;
+
 /// A command.
 export type Command = (
   message: Message,
@@ -11,20 +48,30 @@ export type Command = (
   opts: Record<string, string>
 ) => Promise<void>;
 
-export type CommandArg = Word | CodeBlock | DiscordObject;
+// Some convenience functions to get the actual values used in commands.
 
-export type Word = {
-  type: "word";
-  word: string;
-};
+const anyWord = (_: string) => true;
+export const word = (validator: (w: string) => boolean = anyWord) =>
+  wordSchema()
+    .refine((w) => validator(w.word))
+    .transform((w) => w.word);
 
-export type CodeBlock = {
-  type: "code";
-  info: string;
-  code: string;
-};
+export const discordUser = (client: Client) =>
+  userTagSchema()
+    .refine(
+      (u) => client.users.cache.get(u.id) !== undefined,
+      (u) => ({
+        message: `Could not find user with ID: ${u.id}`,
+      })
+    )
+    .transform((u) => client.users.cache.get(u.id)!);
 
-export type DiscordObject = {
-  type: "user" | "role" | "channel";
-  id: string;
-};
+export const discordTextChannel = (client: Client) =>
+  channelTagSchema()
+    .refine(
+      (c) => client.channels.cache.get(c.id) instanceof TextChannel,
+      (c) => ({
+        message: `Could not find text channel with ID: ${c.id}`,
+      })
+    )
+    .transform((u) => client.channels.cache.get(u.id)! as TextChannel);
