20509d428b97732dc15da28303775b807f37ef3e
Kacarott <keldan.chapman@gmail.com>
2022-01-07 00:14:48 +0100

    Add ?link command

--- src/events/message/commands/index.ts
+++ src/events/message/commands/index.ts
@@ -5,11 +5,13 @@ import ping from "./dev/ping";
 import dump from "./dev/dump";
 import introduce from "./help/introduce";
 import warn from "./moderation/warn";
+import link from "./help/link";
 
 const commands: { [k: string]: Command } = {
   ping,
   dump,
   introduce,
   warn,
+  link,
 };
 export default commands;
