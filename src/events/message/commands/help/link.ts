2bb2d81ed471d99c7c03093e21239b75f9dde0ff
Kacarott <keldan.chapman@gmail.com>
2022-01-07 08:43:58 +0100

    Add clans link

--- src/events/message/commands/help/link.ts
+++ src/events/message/commands/help/link.ts
@@ -11,6 +11,7 @@ const LINKS: { [k: string]: string } = {
   ranking: "https://docs.codewars.com/curation/kata",
   troubleshooting: "https://docs.codewars.com/training/troubleshooting",
   github: "https://github.com/codewars/",
+  clans: "https://docs.codewars.com/community/following/#clans",
 };
 
 const USAGE: string = `Usage: \`?link {${Object.keys(LINKS).join("|")}}\``;
