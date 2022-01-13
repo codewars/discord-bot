7a6a5c5df27ad23a93777b38d53a2a6f6a88d291
Keldan Chapman <43819332+Kacarott@users.noreply.github.com>
2022-01-13 10:21:27 +0100

    Refactor to use await
    Co-authored-by: kazk <kazk.dev@gmail.com>

--- src/codewars.ts
+++ src/codewars.ts
@@ -29,10 +29,15 @@ type ProfileResponse = z.infer<typeof ProfileResponse>;
 // Retrieve a users language score, or overall score if language is undefined
 // Throws error if a user is not found
 export async function getUser(user: string, lang?: string): Promise<number> {
-  return await fetch("https://www.codewars.com/api/v1/users/" + user)
-    .then((response: any): any => (response.status === 404 ? { success: false } : response.json()))
-    .then((result: any): any => {
-      ProfileResponse.parse(result);
-      return (lang ? result.ranks.languages[lang] : result.ranks.overall)?.score ?? 0;
-    });
+  const response = await fetch("https://www.codewars.com/api/v1/users/" + user);
+  if (response.status === 404) throw new UserNotFoundError(user);
+
+  const result = ProfileResponse.parse(await response.json());
+  return (lang ? result.ranks.languages[lang] : result.ranks.overall)?.score ?? 0;
+}
+
+export class UserNotFoundError extends Error {
+  constructor(user: string) {
+    super(`user ${user} not found`);
+  }
 }
