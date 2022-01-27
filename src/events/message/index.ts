afc9d6d6451766cb423eaac76e4224514847b188
Donald Sebastian Leung <donaldsebleung@gmail.com>
2022-01-28 04:15:27 +0800

    Add ?help command for listing existing bot commands (#29)

--- src/events/message/index.ts
+++ src/events/message/index.ts
@@ -5,6 +5,14 @@ import handlers from "./handlers";
 
 const PREFIX = process.env.COMMAND_PREFIX || "?";
 
+const HELP: string = `The following commands are available:
+
+${Object.keys(commands)
+  .map((name) => `- \`${PREFIX}${name}\``)
+  .join("\n")}
+
+Note that some commands are only available to privileged users.`;
+
 // `client` can be accessed from `message.client`.
 export const onMessage = async (message: Message) => {
   // Never react to bots
@@ -15,6 +23,11 @@ export const onMessage = async (message: Message) => {
     const rest = content.slice(PREFIX.length);
     const name = rest.match(/^\S+/)?.[0];
     // Run command and finish if known, otherwise ignore.
+    // The help command is special and lists all other existing commands.
+    if (name === "help") {
+      message.channel.send(HELP);
+      return;
+    }
     if (name && commands.hasOwnProperty(name)) {
       const body = rest.slice(name.length).trimLeft();
       const { args, options } = parseArguments(body);
