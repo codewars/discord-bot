5a04b2ac35e61498d181ea063d2f1be9a763ce3b
Donald Leung <donaldleung@cre.com.hk>
2022-01-28 12:10:40 +0800

    Add bot status to display help command

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,8 +1,13 @@
 import { Client } from "discord.js";
+import { PREFIX } from "./message";
 
 export const makeOnReady = (bot: Client) => async () => {
   console.log("ready!");
   await bot.user?.setPresence({
     status: "online",
+    activity: {
+      name: `${PREFIX}help`,
+      type: "PLAYING",
+    },
   });
 };
