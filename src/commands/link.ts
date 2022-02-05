64d35e02d9f74af8e7c0c212b2f0a40a7f9d8906
Donald Leung <donaldleung@cre.com.hk>
2022-02-05 18:41:35 +0800

    Add link descriptions; add optional parameter to mention user

--- src/commands/link.ts
+++ src/commands/link.ts
@@ -1,19 +1,55 @@
 import { CommandInteraction } from "discord.js";
-import { SlashCommandBuilder } from "@discordjs/builders";
+import { SlashCommandBuilder, hyperlink, hideLinkEmbed, userMention } from "@discordjs/builders";
 
-const LINKS: { [k: string]: string } = {
-  docs: "https://docs.codewars.com/",
-  honor: "https://docs.codewars.com/gamification/honor",
-  kata: "https://www.codewars.com/kata",
-  katas: "https://www.codewars.com/kata",
-  authoring: "https://docs.codewars.com/authoring",
-  rank: "https://docs.codewars.com/curation/kata",
-  ranking: "https://docs.codewars.com/curation/kata",
-  troubleshooting: "https://docs.codewars.com/training/troubleshooting",
-  debug: "https://docs.codewars.com/training/troubleshooting",
-  github: "https://github.com/codewars/",
-  clans: "https://docs.codewars.com/community/following/#clans",
-  vim: "https://docs.codewars.com/references/kata-trainer/#editors",
+const LINKS: { [k: string]: { description: string; url: string } } = {
+  docs: {
+    description: "The Codewars Docs",
+    url: "https://docs.codewars.com/",
+  },
+  honor: {
+    description: "Honor | The Codewars Docs",
+    url: "https://docs.codewars.com/gamification/honor",
+  },
+  kata: {
+    description: "Kata | Codewars",
+    url: "https://www.codewars.com/kata",
+  },
+  katas: {
+    description: "Kata | Codewars",
+    url: "https://www.codewars.com/kata",
+  },
+  authoring: {
+    description: "Authoring Content | The Codewars Docs",
+    url: "https://docs.codewars.com/authoring",
+  },
+  rank: {
+    description: "Reviewing a Kata",
+    url: "https://docs.codewars.com/curation/kata",
+  },
+  ranking: {
+    description: "Reviewing a Kata",
+    url: "https://docs.codewars.com/curation/kata",
+  },
+  troubleshooting: {
+    description: "Troubleshooting Your Solution | The Codewars Docs",
+    url: "https://docs.codewars.com/training/troubleshooting",
+  },
+  debug: {
+    description: "Troubleshooting Your Solution | The Codewars Docs",
+    url: "https://docs.codewars.com/training/troubleshooting",
+  },
+  github: {
+    description: "Codewars on GitHub",
+    url: "https://github.com/codewars/",
+  },
+  clans: {
+    description: "Clans | The Codewars Docs",
+    url: "https://docs.codewars.com/community/following/#clans",
+  },
+  vim: {
+    description: "Editors | The Codewars Docs",
+    url: "https://docs.codewars.com/references/kata-trainer/#editors",
+  },
 };
 
 // link
@@ -27,9 +63,16 @@ export const data = new SlashCommandBuilder()
       .setRequired(true)
       .addChoices(Object.keys(LINKS).map((k) => [k, k]))
   )
+  .addUserOption((option) =>
+    option.setName("target").setDescription("Direct the specified user to the given link")
+  )
   .toJSON();
 
 export const call = async (interaction: CommandInteraction) => {
   const topic = interaction.options.getString("topic", true);
-  await interaction.reply(`See <${LINKS[topic]}>`);
+  const target = interaction.options.getUser("target");
+  const linkInfo = LINKS[topic];
+  let replyMsg = `See ${hyperlink(linkInfo.description, hideLinkEmbed(linkInfo.url))}`;
+  if (target) replyMsg = `${userMention(target.id)} ${replyMsg}`;
+  await interaction.reply(replyMsg);
 };
