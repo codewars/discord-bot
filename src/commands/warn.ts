d867155085bc0397354ab4fbd702fcbbde89980b
Donald Leung <donaldleung@cre.com.hk>
2022-02-10 23:09:04 +0800

    Replace ?warn with /warn

---
+++ src/commands/warn.ts
@@ -0,0 +1,78 @@
+import { CommandInteraction } from "discord.js";
+import { SlashCommandBuilder, userMention, hideLinkEmbed } from "@discordjs/builders";
+import { getTexts } from "../common";
+
+const CONDUCT = "conduct";
+const CONTENT = "content";
+const SPAM = "spam";
+type Reason = typeof CONDUCT | typeof CONTENT | typeof SPAM;
+
+const reasons = [
+  {
+    name: CONDUCT,
+    description: "Poor conduct: harassment, witch hunting, sexism, etc.",
+  },
+  {
+    name: CONTENT,
+    description: "Offensive / NSFW content: nudity, sex, violence, etc.",
+  },
+  {
+    name: SPAM,
+    description:
+      "Spam / phishing: unauthorized server invites, advertisements, malicious links etc.",
+  },
+];
+
+const warnTexts: Map<string, string> = getTexts(
+  "warn",
+  reasons.map((reason) => reason.name)
+);
+
+// warn
+export const data = new SlashCommandBuilder()
+  .setName("warn")
+  .setDescription("Warn the user for violation of server rules")
+  .setDefaultPermission(false)
+  .addUserOption((option) =>
+    option.setName("user").setDescription("The user who violated server rules").setRequired(true)
+  )
+  .addStringOption((option) =>
+    option
+      .setName("reason")
+      .setDescription("The reason the specified user violated server rules")
+      .setRequired(true)
+      .addChoices(reasons.map((reason) => [reason.description, reason.name]))
+  )
+  .toJSON();
+
+export const authorizedRoles = ["admin", "mods"];
+
+export const call = async (interaction: CommandInteraction) => {
+  const user = interaction.options.getUser("user", true);
+  const reason = interaction.options.getString("reason", true) as Reason;
+  try {
+    const reply = warnTexts.get(reason);
+    if (!reply) {
+      await interaction.reply({
+        content: `Could not get the text for reason "${reason}"
+
+Text is available for the following reasons:
+
+${reasons.map((reason) => `- ${reason.name}`).join("\n")}`,
+        ephemeral: true,
+      });
+      return;
+    }
+    const dm = await user.createDM();
+    await dm.send(reply);
+    await interaction.reply(
+      `${userMention(user.id)} You've violated server rules, please check your DMs now`
+    );
+  } catch (err) {
+    await interaction.reply(
+      `${userMention(user.id)} You've violated server rules, see ${hideLinkEmbed(
+        `https://github.com/codewars/discord-bot/blob/main/text/warn/${reason}.md`
+      )} for details.`
+    );
+  }
+};
--- src/common.ts
+++ src/common.ts
@@ -1,18 +1,7 @@
 import { readFileSync } from "fs";
 import * as path from "path";
-import { Role, Message } from "discord.js";
-
-const isModerator = (role: Role) => role.name === "admin" || role.name === "mods";
 
 const textPath = path.join(__dirname, "../text");
-
-export const fromModerator = (message: Message): boolean => {
-  const author = message.guild?.members.cache.get(message.author.id);
-  if (!author) return false;
-  const isPrivileged = author.roles.cache.some(isModerator);
-  if (!isPrivileged) return false;
-  return true;
-};
 
 export const getTexts = (commandName: string, values: string[]): Map<string, string> => {
   const commandPath = path.join(textPath, commandName);
