ea8857d58555baf02d6c940807523653f7e6f0bd
hobovsky <hobson@wp.pl>
2023-06-09 17:51:12 +0200

    Refactor the factory to just data

--- src/commands/howto.ts
+++ src/commands/howto.ts
@@ -10,37 +10,87 @@ import {
 } from "discord.js";
 
 // howto
+
+const commands: HowtoCommand[] = [
+  {
+    name: "format_code",
+    description: "How to use code formatting in Discord messages",
+    body: `# How to use code formatting in Discord messages
+Discord supports Markdown for code blocks and syntax highlighting of various programming languages.
+If you want to post code and use syntax highlighting, you need to surround it with three backticks,
+with an optional name of your language:
+
+\\\`\\\`\\\`python
+def hello_world():
+  print("Hello, world!")
+\\\`\\\`\\\`
+
+When you do this, your code will be neatly formatted:
+
+\`\`\`python
+def hello_world():
+  print("Hello, world!")
+\`\`\`
+
+You can replace \`python\` with any other language supported by Discord, or just omit it.
+
+More info in Discord docs: <https://support.discord.com/hc/en-us/articles/210298617-Markdown-Text-101-Chat-Formatting-Bold-Italic-Underline-#h_01GY0DAKGXDEHE263BCAYEGFJA>.`,
+    reactions: [],
+  },
+  {
+    name: "ask_for_help",
+    description: "How to ask for help in a way others will want to help you",
+    body: `# How to ask for help
+- Ask your question in an appropriate channel. You can use \`#help-solve\` for a general help with some kata, or topic-specific channels ( \`#python\`, or \`#algorithms\` ) if you need help with an algorithm or a syntax of a language.
+- Create a thread to keep the discussion focused on your topic (react with :thread: \`:thread:\` or use \`/howto create_thread\` for more info).
+- Explain what kata you are trying to solve. Post its title, and a link to its description (react with :link: \`:link:\` or use \`/howto kata_link\` for more info).
+- Explain what your problem is. Tests do not accept your answers? Solution is timing out? Is there some other problem?
+- Post **properly formatted** code of your solution (react with :hash: \`:hash:\` or use \`/howto format_code\` for more info).
+- If the code you posted contains spoilers for the discussed kata, please delete it after discussing your matter and getting all necessary help.`,
+    reactions: [
+      { emoji: "🧵", command: "create_thread" },
+      { emoji: "🔗", command: "post_link" },
+      { emoji: "#️⃣", command: "format_code" },
+    ],
+  },
+  {
+    name: "post_link",
+    description: "How to post links to kata",
+    body: `# How to post links to kata
+- Surround the links to kata with angle brackets: \`<https://www.codewars.com/kata/50654ddff44f800200000004/python>\`. This will prevent Discord from inserting embeds into your message, as they do not carry too much useful information, and are quite annoying.
+- Please remember to not post links directly to kata trainer. Make sure that links posted by you do not end with \`/trainer\`, but lead to a kata description.
+- Remember to also mention a title of the kata you are linking to.`,
+    reactions: [],
+  },
+  {
+    name: "create_thread",
+    description: "How to create discord threads",
+    body: `# How to create Discord threads
+Please use Discord threads to keep discussions focused, avoid spoilers, and to not introduce unnecessary noise into main channels. Remember to give a meaningful title to your thread, for example _"hobovsky's thread - help to solve Multiply kata"_, or something similar.
+
+See Discord docs if you do not know how to create threads: <https://support.discord.com/hc/en-us/articles/4403205878423-Threads-FAQ#h_01GDXVYE6AZA3QRSB42F5AGGYG>`,
+    reactions: [],
+  },
+];
+
 export const data = async () => {
   const addTargetUserParam = (scb: SlashCommandSubcommandBuilder) =>
     scb.addUserOption((b) =>
       b.setName("user").setDescription("User to send the message to").setRequired(false)
     );
 
-  return new SlashCommandBuilder()
+  let rootCommandBuilder = new SlashCommandBuilder()
     .setName("howto")
-    .setDescription("HOWTOs and tutorials")
-    .addSubcommand((scb) =>
-      addTargetUserParam(
-        scb.setName("format_code").setDescription("How to use code formatting in Discord messages")
-      )
-    )
-    .addSubcommand((scb) =>
-      addTargetUserParam(
-        scb
-          .setName("ask_for_help")
-          .setDescription("How to ask for help in a way others will want to help you")
-      )
-    )
-    .addSubcommand((scb) =>
-      addTargetUserParam(scb.setName("post_link").setDescription("How to post links to kata"))
-    )
-    .addSubcommand((scb) =>
-      addTargetUserParam(
-        scb.setName("create_thread").setDescription("How to create discord threads")
-      )
-    )
-    .toJSON();
+    .setDescription("HOWTOs and tutorials");
+
+  for (let subcommand of commands) {
+    rootCommandBuilder.addSubcommand((scb) =>
+      addTargetUserParam(scb.setName(subcommand.name).setDescription(subcommand.description))
+    );
+  }
+  return rootCommandBuilder.toJSON();
 };
+
 const allowedRoles = new Set(["admin", "mods", "mods+", "power-users"]);
 
 const hasSufficientPrivilege = (member: GuildMember | APIInteractionGuildMember | null) => {
@@ -63,12 +113,21 @@ export const call = async (interaction: ChatInputCommandInteraction) => {
   let selfTarget = targetUser.id === invokingUser.id;
   if (selfTarget || hasSufficientPrivilege(interaction.member)) {
     let subCommand = interaction.options.getSubcommand();
-    postHowtoDm(subCommand, targetUser).then(() =>
+    let dmReply = commands.find((c) => c.name == subCommand);
+
+    if (dmReply) {
+      postHowtoDm(dmReply, targetUser).then(() =>
+        interaction.reply({
+          content: `${userMention(targetUser.id)} please check your DMs`,
+          ephemeral: selfTarget,
+        })
+      );
+    } else {
       interaction.reply({
-        content: `${userMention(targetUser.id)} please check your DMs`,
-        ephemeral: selfTarget,
-      })
-    );
+        content: `Unknown command: \`${subCommand}\``,
+        ephemeral: true,
+      });
+    }
   } else {
     interaction.reply({
       content: `${userMention(invokingUser.id)}, you are not privileged to use this command.`,
@@ -77,23 +136,22 @@ export const call = async (interaction: ChatInputCommandInteraction) => {
   }
 };
 
-const postHowtoDm = async (command: string, targetUser: User) => {
-  let reply = ReplyBuilder.for(command).buildReply();
-
+const postHowtoDm = async (command: HowtoCommand, targetUser: User) => {
   const setUpReactions = (msg: Message<false>) => {
-    for (let reaction of reply.reactions) {
+    for (let reaction of command.reactions) {
       msg.react(reaction.emoji);
     }
 
     // Do not set up a collector if there are no reactions
-    if (!reply.reactions.length) return;
+    if (!command.reactions.length) return;
 
     const collector = msg.createReactionCollector({ filter: (_, reactor) => !reactor.bot });
     collector.on("collect", (reaction, reactor) => {
       if (reactor.bot || reactor.id != targetUser.id) return;
 
       let emoji = reaction.emoji.name;
-      let reactionCommand = reply.reactions.find((r) => r.emoji == emoji)?.command;
+      let commandName = command.reactions.find((r) => r.emoji == emoji)?.command;
+      let reactionCommand = commands.find((c) => c.name == commandName);
       if (reactionCommand) {
         postHowtoDm(reactionCommand, targetUser);
       }
@@ -102,7 +160,7 @@ const postHowtoDm = async (command: string, targetUser: User) => {
 
   return targetUser
     .createDM(true)
-    .then((dm) => dm.send(reply.body))
+    .then((dm) => dm.send(command.body))
     .then(setUpReactions);
 };
 
@@ -116,111 +174,16 @@ class Reaction {
   public readonly command: string;
 }
 
-class HowtoReply {
-  constructor(body: string, reactions: Reaction[]) {
+class HowtoCommand {
+  constructor(name: string, description: string, body: string, reactions: Reaction[]) {
+    this.name = name;
+    this.description = description;
     this.body = body;
     this.reactions = reactions;
   }
 
+  public readonly name: string;
+  public readonly description: string;
   public readonly body: string;
   public readonly reactions: Reaction[];
-}
-
-interface IReplyBuilder {
-  buildReply(): HowtoReply;
-}
-
-class ReplyBuilder {
-  static for(topic: string): IReplyBuilder {
-    switch (topic) {
-      case "format_code":
-        return new FormatCodeReplyBuilder();
-      case "ask_for_help":
-        return new AskForHelpReplyBuilder();
-      case "post_link":
-        return new PostKataLinkReplyBuilder();
-      case "create_thread":
-        return new CreateThreadReplyBuilder();
-      default:
-        return new DummyReplyBuilder();
-    }
-  }
-}
-
-class FormatCodeReplyBuilder implements IReplyBuilder {
-  buildReply(): HowtoReply {
-    return {
-      reactions: [],
-      body: `# How to use code formatting in Discord messages
-Discord supports Markdown for code blocks and syntax highlighting of various programming languages.
-If you want to post code and use syntax highlighting, you need to surround it with three backticks,
-with an optional name of your language:
-
-\\\`\\\`\\\`python
-def hello_world():
-  print("Hello, world!")
-\\\`\\\`\\\`
-
-When you do this, your code will be neatly formatted:
-
-\`\`\`python
-def hello_world():
-  print("Hello, world!")
-\`\`\`
-
-You can replace \`python\` with any other language supported by Discord, or just omit it.
-
-More info in Discord docs: <https://support.discord.com/hc/en-us/articles/210298617-Markdown-Text-101-Chat-Formatting-Bold-Italic-Underline-#h_01GY0DAKGXDEHE263BCAYEGFJA>.`,
-    };
-  }
-}
-
-class AskForHelpReplyBuilder implements IReplyBuilder {
-  buildReply(): HowtoReply {
-    return {
-      reactions: [
-        { emoji: "🧵", command: "create_thread" },
-        { emoji: "🔗", command: "post_link" },
-        { emoji: "#️⃣", command: "format_code" },
-      ],
-      body: `# How to ask for help
-- Ask your question in an appropriate channel. You can use \`#help-solve\` for a general help with some kata, or topic-specific channels ( \`#python\`, or \`#algorithms\` ) if you need help with an algorithm or a syntax of a language.
-- Create a thread to keep the discussion focused on your topic (react with :thread: \`:thread:\` or use \`/howto create_thread\` for more info).
-- Explain what kata you are trying to solve. Post its title, and a link to its description (react with :link: \`:link:\` or use \`/howto kata_link\` for more info).
-- Explain what your problem is. Tests do not accept your answers? Solution is timing out? Is there some other problem?
-- Post **properly formatted** code of your solution (react with :hash: \`:hash:\` or use \`/howto format_code\` for more info).
-- If the code you posted contains spoilers for the discussed kata, please delete it after discussing your matter and getting all necessary help.
-`,
-    };
-  }
-}
-
-class PostKataLinkReplyBuilder implements IReplyBuilder {
-  buildReply(): HowtoReply {
-    return {
-      reactions: [],
-      body: `# How to post links to kata
-- If possible, post links in a way they do not result in large embeds. Embeds are not very helpful, they do not carry too much useful information, and are quite annoying. To prevent Discord from adding embeds to your kata links, surround the links with angle brackets: \`<https://www.codewars.com/kata/50654ddff44f800200000004/python>\`.
-- Please remember to not post links directly to kata trainer. Make sure that links posted by you do not end with \`/trainer\`, but lead to a kata description.
-- Remember to also mention a title of the kata you are linking to.`,
-    };
-  }
-}
-
-class CreateThreadReplyBuilder implements IReplyBuilder {
-  buildReply(): HowtoReply {
-    return {
-      reactions: [],
-      body: `# How to create Discord threads
-Please use Discord threads to keep discussions focused, avoid spoilers, and to not introduce unnecessary noise into main channels. Remember to give a meaningful title to your thread, for example _"hobovsky's thread - help to solve Multiply kata"_, or something similar.
-
-See Discord docs if you do not know how to create threads: <https://support.discord.com/hc/en-us/articles/4403205878423-Threads-FAQ#h_01GDXVYE6AZA3QRSB42F5AGGYG>`,
-    };
-  }
-}
-
-class DummyReplyBuilder implements IReplyBuilder {
-  buildReply(): HowtoReply {
-    return { reactions: [], body: "Unknown topic" };
-  }
 }
